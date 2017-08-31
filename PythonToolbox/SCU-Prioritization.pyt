cd1bc8b4c674e675984835845312ecf6b54a5c42
Kirsten Hazler <kirsten.hazler@dcr.virginia.gov>
2017-08-31 16:18:54 -0400

    A few tweaks in libScuFx, and developing SCU prioritization toolbox.

--- PythonToolbox/SCU-Prioritization.pyt
+++ PythonToolbox/SCU-Prioritization.pyt
@@ -51,7 +51,7 @@ class Toolbox(object):
       self.alias = "SCU Prioritization Toolbox"
 
       # List of tool classes associated with this toolbox
-      self.tools = [flowDistBuffer]
+      self.tools = [flowDistBuffer, scuPrior]
       
 # Define the tools
 class flowDistBuffer(object):
@@ -63,12 +63,12 @@ class flowDistBuffer(object):
 
    def getParameterInfo(self):
       """Define parameters"""
-      parm0 = defineParam("in_Feats", "Input SCU features", "GPFeatureLayer", "Required", "Input")
+      parm0 = defineParam("in_Feats", "Input features", "GPFeatureLayer", "Required", "Input")
       parm1 = defineParam("fld_ID", "Unique ID field (integer)", "String", "Required", "Input")
       parm2 = defineParam("in_FlowDir", "Input flow direction raster", "GPRasterLayer", "Required", "Input")
-      parm3 = defineParam("out_Buff", "Output flow distance buffers", "DEFeatureClass", "Required", "Output")
+      parm3 = defineParam("out_Feats", "Output flow distance buffers", "DEFeatureClass", "Required", "Output")
       parm4 = defineParam("maxDist", "Maximum distance", "GPDouble", "Required", "Input")
-      parm4.value = 500
+      parm4.value = 250
       parm5 = defineParam("out_Scratch", "Scratch geodatabase", "DEWorkspace", "Optional", "Input")
       parm5.filter.list = ["Local Database"]
       parms = [parm0, parm1, parm2, parm3, parm4, parm5]
@@ -97,13 +97,74 @@ class flowDistBuffer(object):
       """The source code of the tool."""
       # Set up parameter names and values
       declareParams(parameters)
-      mDist = float(maxDist) # Convert string back to number
+      mDist = float(maxDist) # Convert distance string back to number
 
       if out_Scratch != 'None':
          scratchParm = out_Scratch 
       else:
          scratchParm = "in_memory" 
       
-      delinFlowDistBuff(in_Feats, fld_ID, in_FlowDir, out_Buff, mDist, scratchParm)
+      delinFlowDistBuff(in_Feats, fld_ID, in_FlowDir, out_Feats, mDist, scratchParm)
 
-      return out_Buff
+      return out_Feats
+      
+class scuPrior(object):
+   def __init__(self):
+      """Prioritizes Stream Conservation Units (SCUs) for conservation."""
+      self.label = "Prioritize Stream Conservation Units"
+      self.description = ""
+      self.canRunInBackground = True
+
+   def getParameterInfo(self):
+      """Define parameters"""
+      parm0 = defineParam("in_Feats", "Input buffered SCU features", "GPFeatureLayer", "Required", "Input")
+      parm1 = defineParam("fld_ID", "Unique ID field (integer)", "String", "Required", "Input")
+      parm2 = defineParam("fld_BRANK", "BRANK field", "String", "Required", "Input")
+      parm3 = defineParam("lo_BRANK", "Lowest BRANK to consider", "String", "Required", "Input")
+      parm3.value = 'B2'
+      parm4 = defineParam("in_Integrity", "Input Watershed Integrity Raster", "GPRasterLayer", "Required", "Input")
+      parm5 = defineParam("lo_Integrity", "Minimum integrity value to consider", "GPLong", "Required", "Input")
+      parm5.value = 80
+      parm6 = defineParam("in_ConsPriority", "Input Conservation Priority Raster", "GPRasterLayer", "Required", "Input")
+      parm7 = defineParam("in_Vulnerability", "Input Development Vulnerability Raster", "GPRasterLayer", "Required", "Input")
+      parm8 = defineParam("out_Feats", "Output scored features", "GPFeatureLayer", "Required", "Output")
+      parm9 = defineParam("out_Scratch", "Scratch geodatabase", "DEWorkspace", "Optional", "Input")
+      parm9.filter.list = ["Local Database"]
+      parms = [parm0, parm1, parm2, parm3, parm4, parm5]
+      return parms
+
+   def isLicensed(self):
+      """Set whether tool is licensed to execute."""
+      return True
+
+   def updateParameters(self, parameters):
+      """Modify the values and properties of parameters before internal
+      validation is performed.  This method is called whenever a parameter
+      has been changed."""
+      if parameters[0].altered:
+         fc = parameters[0].valueAsText
+         int_field_names = [f.name for f in arcpy.ListFields(fc, "", "Integer")]
+         parameters[1].filter.list = int_field_names
+         field_names = [f.name for f in arcpy.ListFields(fc)]
+         parameters[2].filter.list = field_names
+      return
+
+   def updateMessages(self, parameters):
+      """Modify the messages created by internal validation for each tool
+      parameter.  This method is called after internal validation."""
+      return
+
+   def execute(self, parameters, messages):
+      """The source code of the tool."""
+      # Set up parameter names and values
+      declareParams(parameters)
+      loInteg = float(lo_Integrity) # Convert value string back to number
+
+      if out_Scratch != 'None':
+         scratchParm = out_Scratch 
+      else:
+         scratchParm = "in_memory" 
+      
+      prioritizeSCUs(in_Feats, fld_ID, fld_BRANK, lo_BRANK, in_Integrity, loInteg, in_ConsPriority, in_Vulnerability, out_Feats, out_Scratch)
+
+      return out_Feats
--- PythonToolbox/libScuFx.py
+++ PythonToolbox/libScuFx.py
@@ -41,7 +41,7 @@ def delinFlowDistBuff(in_Feats, fld_ID, in_FlowDir, out_Feats, maxDist, out_Scra
 
    # Set environment setting and other variables
    arcpy.env.snapRaster = in_FlowDir
-   procDist = 3*maxDist
+   procDist = 2*maxDist
 
    # Check if input features and input flow direction have same spatial reference.
    # If so, just make a copy. If not, reproject features to match raster.
@@ -187,26 +187,72 @@ def delinFlowDistBuff(in_Feats, fld_ID, in_FlowDir, out_Feats, maxDist, out_Scra
       printWrng('These features may be incorrect: %s' % str(flags))
    return out_Feats
 
-def prioritizeSCUs(in_SCU, in_Catch, fld_ID, fld_BRANK, lo_BRANK, in_Integrity, in_ConsPriority, in_Vulnerability, out_SCU, out_Scratch = 'in_memory'):
+def prioritizeSCUs(in_Feats, fld_ID, fld_BRANK, lo_BRANK, in_Integrity, lo_Integrity, in_ConsPriority, in_Vulnerability, out_Feats, out_Scratch = 'in_memory'):
    '''Prioritizes Stream Conservation Units (SCUs) for conservation, based on biodiversity rank (BRANK), watershed integrity and conservation priority (from ConservationVision Watershed Model), and vulnerability (from ConservationVision Development Vulnerability Model)'''
-   # Step 1: First cut based on BRANK: Create subset of SCUs ranked lo_BRANK or better
+   # Step 1: First cut: Create subset of buffered SCUs ranked lo_BRANK or better
    selQry = "%s <= '%s'" % (fld_BRANK, lo_BRANK)
-   arcpy.Select_analysis (in_SCU, out_SCU, selQry)
+   arcpy.Select_analysis (in_Feats, out_Feats, selQry)
    
-   # Step 2: For each SCU catchment corresponding to SCUs in subset, get zonal stats of Watershed Integrity, Conservation Priority and Vulnerability. Do this in loop in case of catchment overlap.
-   # Process: Add fields (WtrshdInteg, ConsPrior, and Vuln)
+   # Step 2: For each buffered SCU in subset, get zonal stats for Watershed Integrity, Conservation Priority and Vulnerability. Do this in loop in case of buffer overlap.
+      
+   # Process: Add fields to hold the stats
+   for f in ['INTEG', 'CPRIOR', 'VULN']:
+      arcpy.AddField_management (out_Feats, f, 'FLOAT')
+      
+   # Set up some variables used in loop
+   tmpFeat = out_Scratch + os.sep + 'tmpFeat' # temp feature class 
+   tmpTab = out_Scratch + os.sep + 'tmpTab' # temp table 
+   flags = [] # empty list to keep track of suspects
    
-   # Set up cursor for loop
-   #for s in SCUs:
-      # Process: Select (catchment)
-      # Process: Zonal statistics by table (Watershed Integrity)
-      # Get mean value and update ConsPrior field
-      # Process: Zonal statistics by table (Conservation Priority)
-      # Get mean value and update ConsPrior field
-      # Process: Zonal statistics by table (Vulnerability)
-      # Get mean value and update Vuln field
+   # Set up processing cursor and loop
+   cursor = arcpy.da.UpdateCursor(out_Feats, [fld_ID, 'INTEG', 'CPRIOR', 'VULN'])
+   for row in cursor:
+      try:
+         myID = row[0]
+         
+         # Make temporary feature class
+         selQry = "%s <= %s" % (fld_ID, str(myID))
+         arcpy.Select_analysis (out_Feats, tmpFeat, selQry)
+         
+         # Run zonal stats, extract values, and update fields
+         outTab = ZonalStatisticsAsTable (tmpFeat, fld_ID, in_Integrity, tmpTab, 'DATA', 'MEAN')
+         val = arcpy.SearchCursor(tmpTab).next().MEAN
+         row[1] = val
+         
+         outTab = ZonalStatisticsAsTable (tmpFeat, fld_ID, in_ConsPriority, tmpTab, 'DATA', 'MEAN')
+         val = arcpy.SearchCursor(tmpTab).next().MEAN
+         row[2] = val
+         
+         outTab = ZonalStatisticsAsTable (tmpFeat, fld_ID, in_Vulnerability, tmpTab, 'DATA', 'MEAN')
+         val = arcpy.SearchCursor(tmpTab).next().MEAN
+         row[3] = val
+         
+         cursor.updateRow(row)
+         
+         # Reset extent, because Arc is stupid.
+         arcpy.env.extent = "MAXOF"
+         
+      except:
+      # Add failure message and append failed feature ID to list
+         printMsg("\nFailed to fully process feature " + str(myID))
+         myFailList.append(myID)
+
+         # Error handling code swiped from "A Python Primer for ArcGIS"
+         tb = sys.exc_info()[2]
+         tbinfo = traceback.format_tb(tb)[0]
+         pymsg = "PYTHON ERRORS:\nTraceback Info:\n" + tbinfo + "\nError Info:\n " + str(sys.exc_info()[1])
+         msgs = "ARCPY ERRORS:\n" + arcpy.GetMessages(2) + "\n"
+
+         printWrng(msgs)
+         printWrng(pymsg)
+         printMsg(arcpy.GetMessages(1))
+
+         # Add status message
+         printMsg("\nMoving on to the next feature.  Note that the output will be incomplete.")
 
    # Step 3: Score catchments based on BRANK, Watershed Integrity, Conservation Priority, and Vulnerability, then rank
+   codeblock = '''def score(brank, integ, crprior, vuln):
+      if brank'''
 
 # Use the main function below to run the flow distance buffer function directly from Python IDE with hard-coded variables
 def main():
@@ -214,7 +260,7 @@ def main():
    fld_ID = 'lngID'
    in_FlowDir = r'H:\Backups\DCR_Work_DellD\GIS_Data_VA_proc\Finalized\NHDPlus_Virginia.gdb\fdir_VA'
    out_Feats = r'C:\Users\xch43889\Documents\Working\SCU_prioritization\SCU_work.gdb\scuFlowBuffs500'
-   maxDist = 500
+   maxDist = 250
    out_Scratch = r'C:\Users\xch43889\Documents\Working\SCU_prioritization\scratch-20170831.gdb'
    # End of user input
 
